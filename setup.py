176223989a1968da3a5120837caebf98040e2bf8
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2021-06-08 02:51:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 import setuptools
 import unittest
 
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(name='easyipc',
     version='0.0.5',
     description='Easy-to-use Python library for inter-process communications.',
@@ -14,6 +20,8 @@ setuptools.setup(name='easyipc',
     package_dir={'easyipc' : 'src'}, 
     test_suite = 'tests',
     install_requires = ['numpy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',             
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
